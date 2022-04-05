02e053911e12108842f2b1115ea98b89b2f35b75
kgisaackg <kg21isaackg21@gmail.com>
2022-04-05 14:07:56 +0200

    profile modified

--- src/app/components/profile-update/profile-update.component.ts
+++ src/app/components/profile-update/profile-update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-update',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileUpdateComponent implements OnInit {
 
-  constructor() { }
+  roles = [
+    { name: "Farmer" },
+    { name: "Farmer Experts"},
+    { name: "Financial Advisor"}
+  ]
+
+  selectedOption = "";
+
+  updateForm = this.formBuilder.group({
+    firstname: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(3)]],
+    lastname: ['', [Validators.required, Validators.minLength(3), Validators.pattern('[a-zA-Z ]*')]],
+    phoneNumber: ['', [Validators.required, Validators.pattern('(((0[6-8]))([0-9]{8}))')]],//^(((0[6-8]))([0-9]{8})) OR ((\\+27))[6-8][0-9]{8}
+    emailAddress: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    role: [this.roles[3], Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
