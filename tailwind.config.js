abaa1fa845d4b2169bb3dc43bb5140985e178fcf
kgisaackg <kg21isaackg21@gmail.com>
2022-05-18 10:59:53 +0200

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
         'cust-green': '#8AA584',
         'cust-green-200': '#6D8368',
         'opacity-green': '#C2D2BE'
-      }
+      },
+      backgroundImage: {
+        'hero-pattern': "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('./assets/image/farm.jpg'))",
+     },
     },
   },
   plugins: [require('daisyui')],
