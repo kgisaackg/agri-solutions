bef70c579b748f213818ba8f577d223680ed8b38
kgissackg <kg21isaackg21@gmail.com>
2022-05-20 15:29:19 +0200

    icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         'opacity-green': '#C2D2BE'
       },
       backgroundImage: {
-        'hero-pattern': "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('./assets/image/farm.jpg'))",
+        'hero-pattern': "linear-gradient(to right bottom, rgba('#28b485',0.8), rgba('#7ed56f',0.8)), url('./assets/image/farm.jpg'))",
      },
     },
   },
